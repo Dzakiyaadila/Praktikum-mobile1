38eeaeb459c156cd7c4bc84e7bd427f920980d9c
Dzakiyaadila <dzakiyaadila4@gmail.com>
2025-11-18 22:35:24 +0700

    hapus lagi

--- EmployeeApp1/app/src/main/java/com/example/employeapp/model/EmployeeResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.employeapp.model
-
-import com.google.gson.annotations.SerializedName
-
-data class EmployeeResponse(
-    @SerializedName("data")
-    val data: List<Employee>
-)
--- EmployeeApp1/app/src/main/java/com/example/employeapp/model/UpdateEmployeeRequest.kt
+++
@@ -1,8 +0,0 @@
-// UpdateEmployeeRequest.kt
-package com.example.employeapp.model
-
-data class UpdateEmployeeRequest(
-    val name: String? = null,
-    val salary: Int? = null,
-    val age: Int? = null
-)
